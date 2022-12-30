44c092f6b30b69493adb9994786dc5369bc06bbf
Snehau259 <snehau259@gmail.com>
2022-12-30 21:15:13 +0530

    [Sneha] add. rowPost

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import React from 'react';
 import NavBar from './components/navBar/NavBar'
 import Banner from './components/banner/Banner'
+import RowPost from './components/rowPost/RowPost'
 import './App.css'
 import './components/navBar/navBar.css'
-import './components/banner/banner.css'
+import './components/rowPost/RowPost.css'
 
 function App() {
   return (
     <div>
       <NavBar />
       <Banner />
+      <RowPost/>
     </div>
 
   );
